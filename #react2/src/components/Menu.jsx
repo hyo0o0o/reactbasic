e4fb8e5d358009e29372b26e020a29484d898e11
hyo0o0o <129016961+hyo0o0o@users.noreply.github.com>
2023-05-31 17:05:29 +0900

    Add files via upload

---
+++ #react2/src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Menu = ({title}) => {
+    console.log({title})
+  return (
+    <div>{title}Menu</div>
+  )
+}
+
+export default Menu
---
+++ #react2/src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = function ({num,color}) {
+    console.log({num,color})
+  return (
+  <div>나는{num}번{color}머리</div>
+  );
+};
+
+export default Header;
